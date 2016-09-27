60ec7f8d75c398d4ad8e5223374fe441e3879e17
rohit115sharma@gmail.com <rohit@rohit-PC>
2016-09-27 19:35:52 +0530

    different types to create object

--- src/com/test/singleton/MainSingleton.java
+++ src/com/test/singleton/MainSingleton.java
@@ -2,12 +2,17 @@ package com.test.singleton;
 
 public class MainSingleton {
 
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) 
+	{
+		
 		SingletonTest ti=SingletonTest.getInstance();
 		SingletonTest t2=SingletonTest.getInstance();
 		ti.printTestMethod();
 		t2.printTestMethod();
 
 	}
+	
 
 }
+
